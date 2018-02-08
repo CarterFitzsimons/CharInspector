22f5f4129b1efc4f9fe93d8cba56b676b406fc2a
CarterFitzsimons <cfitzsimons1@mail.csuchico.edu>
2018-02-07 23:22:34 -0800

    Used standard library functions to get an inputted characters upper/lowercase value and ASCII value

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
-//Author:
+//Author: Carter Fitzsimons
 #include<iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
+  char input;
 
   cout<<"What character do you want to know about?\n";
+  cin>>input;
 
+  int num = (int)input;
+ 
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
-
-  //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
-
-  //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
+  if(isalpha(num) && isupper(num)){ 
+  cout<<input<<" is an upper case letter!\n";}
   
-  //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  else if (isalpha(num) && isupper(num) == false){ 
+  //when user's entry is between a-z...  
+  cout<<input<<" is a lower case letter!\n";}
+  
+  else if (isalpha(num) != true){ 
+  //in all other cases... 
+  cout<<"?! Pssh. What are you talking about?\n";}
+  
+  //no matter what they enter.. 
+  cout<<"The ASCII value is: "<< num << endl;
 
   return 0;
 }
